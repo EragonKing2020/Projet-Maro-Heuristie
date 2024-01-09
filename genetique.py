e30975d2540f6be0b4836296e9274429fda4c4a4
Fl0r3ntLem <folem0302@gmail.com>
2024-01-09 18:31:59 +0100

    croisement sans correction

--- genetique.py
+++ genetique.py
@@ -1,6 +1,7 @@
 from Flowshop import Flowshop
 from Ordonnancement import Ordonnancement
 import random
+from math import floor
 
 class Genetique():
     def __init__(self,pop_size, flowshop, taux_mutation):
@@ -18,9 +19,28 @@ class Genetique():
             if test == True :
                 self.population[i] = ordo_candidat
                 i+=1
-                
-    # def croisement(self,pos):
+                   
+    def croisement(self,pos):
+        print(self.population)
+        new_population = [None for _ in range(self.taille_population)]
+        for i in range(floor(self.taille_population/2)):
+            parent1 = random.choice(self.population)
+            self.population.remove(parent1)
+            parent2 = random.choice(self.population)
+            self.population.remove(parent2)
+            enfant1 = parent1[:pos] + parent2[pos:]
+            enfant2 = parent2[:pos] + parent1[pos:]
+            new_population[2*i] = enfant1
+            new_population[2*i+1] = enfant2
+        for i in range(self.taille_population):
+            if new_population[len(new_population)-1-i] == None:
+                new_population[len(new_population)-1-i] = self.population[i]
+        # TO DO : correction des enfants
+        self.population = new_population
+            
+
+
 
 if __name__ == "__main__":
     # print(Flowshop.lire_flowshop("jeu2-704.txt").afficher_flowshop())
-    Genetique(10,Flowshop.lire_flowshop("jeu2-704.txt"),0.1)
+    Genetique(11,Flowshop.lire_flowshop("jeu2-704.txt"),0.1).croisement(2)
